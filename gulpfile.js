f483ba74ed8f8de569323cf3006c206428cf4de6
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-01-17 13:35:36 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var wp = require('webpack');
+var rename = require('gulp-rename'); // rename files or directories in streams
+var named = require('vinyl-named'); // needed for gulp-webpack
+var uglify = require('gulp-uglify');
+var path = require('path');
+
+gulp.task('js', function(){
+     return gulp.src(['src/app/module.config.js'], { base: './' })
+        .pipe(named())
+        .pipe(webpack({
+            output: {
+                filename: 'app.bundle.js'
+            },
+            externals: {
+                angular: 'angular',
+                jquery: 'jQuery'
+            },
+            // do external source maps for release, but internal otherwise
+            devtool: (isRelease()) ? '#source-map' : '#inline-source-map',
+            plugins: (isRelease()) ? [uglify] : [],
+            loaders: [
+                {
+                    test: /\.html$/,
+                    loader: "ng-cache?prefix=[dir]/[dir]"
+                }
+            ]
+        }))
+        .pipe(rename({
+            // gulp-webpack doesn't honor base yet so folder is explicit
+            dirname: 'dist/app/scripts'
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.watch('watch', function(){
+    gulp.watch('src/app/**/*.js', ['js']);
+});
+
+function isRelease() {
+    return false;
+}
